92e1fa718967b3ad9e3ddb52ee2f4c14dab9cecd
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-08-14 11:18:48 -0400

    added documentation

--- buildTower.js
+++ buildTower.js
@@ -1,13 +1,13 @@
-function buildTower(n){
-let tower = []
-let towerS = '*'
-for(let i = 0; i<n; i++){ 
-    tower.push(towerS)
-    towerS += "**"
+function buildTower(n){// function to build tower of stars 
+let tower = []// create tower empty array
+let towerS = '*'// tower stars creation variable
+for(let i = 0; i<n; i++){ // run loop for how many times n equals
+    tower.push(towerS)// push tower stars to tower array
+    towerS += "**"// add more stars for next floor
     for (let j = 0; j<i; j++){
-        tower[j] = " " + tower[j] + ' '
+        tower[j] = ' ' + tower[j] + ' '// add spaces to each side
       }
     }
-    return tower
+    return tower// return tower
 }
 console.log(buildTower(8))
